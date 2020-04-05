3adecf6d704a93e35e354f623c430cc60d53117d
Diego Alves <diegoarpalves@gmail.com>
2020-04-06 00:18:51 +0100

    April/2020

--- 1_test_codes/aula_1_print.py
+++ 1_test_codes/aula_1_print.py
@@ -1,7 +1,9 @@
 # Vamos usar a funcao: print
 
-# Primeiro passo: criar uma variante. Lembre-se que em python voce deve substituir os espacos brancos por underscore "_".
+# Primeiro passo: criar uma variante.
+# Lembre-se que em python voce deve substituir os espacos brancos por underscore "_".
 meu_nome = 'Diego_Alves'
 
+# Segundo passo.
 # Mande python imprimir a variante: meu_nome.
 print(meu_nome)
